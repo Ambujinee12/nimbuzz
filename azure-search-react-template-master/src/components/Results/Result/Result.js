ea634728aac761d90f300ce0db9ff57c5930a565
Ambujinee12 <74645186+Ambujinee12@users.noreply.github.com>
2020-11-19 22:30:44 +0530

    Update Result.js

--- azure-search-react-template-master/src/components/Results/Result/Result.js
+++ azure-search-react-template-master/src/components/Results/Result/Result.js
@@ -6,7 +6,7 @@ export default function Result(props) {
     return(
     <div className="card result">
         <a href={`/details/${props.document.metadata_title}`}>
-            <img className="card-img-top" src={props.document.file_path} alt={props.document.file_path}></img>
+            <img className="card-img-top" src={props.document.metadata_title} alt={props.document.file_path}></img>
             <div className="card-body">
                 <h6 className="title-style">{props.document.file_path}</h6>
             </div>

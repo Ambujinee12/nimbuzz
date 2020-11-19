041feaa8e75891f41d74da904520ffae0431dcf9
asthadarshika408 <74617031+asthadarshika408@users.noreply.github.com>
2020-11-19 23:48:37 +0530

    Update Details.js

--- azure-search-react-template-master/src/pages/Details/Details.js
+++ azure-search-react-template-master/src/pages/Details/Details.js
@@ -41,10 +41,8 @@ export default function Details() {
       detailsBody = (
         <div className="card-body">
           <h5 className="card-title">{document.metadata_storage_name}</h5>
-          <img className="image" src={document.file_path} alt="File Path"></img>
           <p className="card-text">{document.filetype?.join('; ')} - {document.equipment}</p>
-          <p className="card-text">PnID {document.PnID}</p>
-          <Rating name="half-rating-read" value={parseInt(document.average_rating)} precision={0.1} readOnly></Rating>
+          <p className="card-text"> PnID {document.PnID}</p>
           <p className="card-text">{document.metadata_storage_file_extension} Extension</p>
         </div>
       );

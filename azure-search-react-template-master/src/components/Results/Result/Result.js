8804b78be034c48b337ed90019116f3ca3e548f8
asthadarshika408 <74617031+asthadarshika408@users.noreply.github.com>
2020-11-21 20:50:37 +0530

    Update Result.js

--- azure-search-react-template-master/src/components/Results/Result/Result.js
+++ azure-search-react-template-master/src/components/Results/Result/Result.js
@@ -8,13 +8,14 @@ export default function Result(props) {
         <a href={`${props.document.file_path}`}>
             <div className="card-body">
                 <h6 className="title-style">{props.document.metadata_storage_name}</h6>
+   </div>
+        </a>
                <p> {props.document.location} </p>
               <p> {props.document.equipment} </p>
               <p> {props.document.filetype} </p>
              <p> {props.document.metadata_storage_last_modified} </p>
              
-            </div>
-        </a>
+         
     </div>
     );
 }

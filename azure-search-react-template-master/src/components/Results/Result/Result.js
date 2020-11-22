0c2d899d2c68b36c5dfb9b6dfbd57b1d12511a28
asthadarshika408 <74617031+asthadarshika408@users.noreply.github.com>
2020-11-22 14:22:38 +0530

    Update Result.js

--- azure-search-react-template-master/src/components/Results/Result/Result.js
+++ azure-search-react-template-master/src/components/Results/Result/Result.js
@@ -4,20 +4,18 @@ import './Result.css';
 
 export default function Result(props) {
     return(
-    <div className="card result">
         <div class="card text-white bg-info mb-3" style="max-width: 25rem;">
         <a href={`${props.document.file_path}`}>
             <div className="card-header">
                 <h6 className="title-style">{props.document.metadata_storage_name}</h6>
-   </div>
+            </div>
         </a>
- <div class="card-body">
-               <p class="card-text">Location : {props.document.location} </p>
-              <p class="card-text">Equipment : {props.document.equipment} </p>
-              <p class="card-text">Filetype : {props.document.filetype} </p>
-             <p class="card-text">Last Modified : {props.document.metadata_storage_last_modified}</p>      
+        <div class="card-body">
+                   <p class="card-text">Location : {props.document.location} </p>
+                  <p class="card-text">Equipment : {props.document.equipment} </p>
+                  <p class="card-text">Filetype : {props.document.filetype} </p>
+                 <p class="card-text">Last Modified : {props.document.metadata_storage_last_modified}</p>      
+        </div>
     </div>
-</div>
-</div>
     );
 }

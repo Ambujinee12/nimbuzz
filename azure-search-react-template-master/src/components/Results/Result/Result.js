52d7731c54b1e2beff69e0d54fc7c714147716ca
asthadarshika408 <74617031+asthadarshika408@users.noreply.github.com>
2020-11-22 14:30:45 +0530

    Update Result.js

--- azure-search-react-template-master/src/components/Results/Result/Result.js
+++ azure-search-react-template-master/src/components/Results/Result/Result.js
@@ -6,8 +6,8 @@ export default function Result(props) {
     return(
         <div class="card text-white bg-info mb-3" style="max-width: 25rem;">
         <a href={`${props.document.file_path}`}>
-            <div className="card-header">
-                <h6 className="title-style">{props.document.metadata_storage_name}</h6>
+            <div class="card-header">
+                <h6 class="title-style">{props.document.metadata_storage_name}</h6>
             </div>
         </a>
         <div class="card-body">

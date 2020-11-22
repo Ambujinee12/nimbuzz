650bbc7c7f40098a10d7eb6b0d5a828ec7c79a7d
asthadarshika408 <74617031+asthadarshika408@users.noreply.github.com>
2020-11-22 15:13:21 +0530

    Update Result.js

--- azure-search-react-template-master/src/components/Results/Result/Result.js
+++ azure-search-react-template-master/src/components/Results/Result/Result.js
@@ -4,18 +4,35 @@ import './Result.css';
 
 export default function Result(props) {
     return(
-        <div className="card text-white bg-info mb-3" style="max-width: 25rem;">
-        <a href={`${props.document.file_path}`}>
+        
+       [
+  'Info'
+  
+].map((variant, idx) => (
+  <Card
+    bg={variant.toLowerCase()}
+    key={idx}
+    text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+    style={{ width: '18rem' }}
+    className="mb-2"
+  >
+    <Card.Header> <a href={`${props.document.file_path}`}>
             <div className="card-header">
                 <h6 className="title-style">{props.document.metadata_storage_name}</h6>
             </div>
-        </a>
-        <div className="card-body">
-                   <p className="card-text">Location : {props.document.location} </p>
+        </a> </Card.Header>
+
+    <Card.Body>
+      <Card.Text>
+                  <p className="card-text">Location : {props.document.location} </p>
                   <p className="card-text">Equipment : {props.document.equipment} </p>
                   <p className="card-text">Filetype : {props.document.filetype} </p>
-                 <p className="card-text">Last Modified : {props.document.metadata_storage_last_modified}</p>      
-        </div>
-    </div>
+                 <p className="card-text">Last Modified : {props.document.metadata_storage_last_modified}</p>     
+      </Card.Text>
+    </Card.Body>
+  </Card>
+));
     );
 }
+
+
